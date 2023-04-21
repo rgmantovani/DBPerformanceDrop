b3e0992a759e813cf7dd76cd7e63076016415bd0
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2023-04-21 19:12:30 -0300

    removing high correlated features

--- 01_createDatasets.R
+++ 01_createDatasets.R
@@ -12,16 +12,21 @@ source("./R/DataPreprocessing.R")
 data.original = read.csv("./data/originalData.csv")
 data.original = data.original[ ,-which(colnames(data.original) %in% DROP.FEATURES)]
 
+# for(cn in colnames(data.original)) {
+# 	cat("* ", cn, " = ", class(data.original[,cn]), "\n")
+# }
+
 # converting some features to the correct type
-data.original = as.numeric(data.original$CPU_LOAD_SHORT)
+data.original$CPU_LOAD_SHORT = as.numeric(data.original$CPU_LOAD_SHORT)
+data.original$TAMANHO_DO_BANCO_GB = as.numeric(gsub(x = data.original$TAMANHO_DO_BANCO_GB, 
+	pattern = ",", replacement = "."))
 
-
-data.filtered = data.original[, EXPERT.CHOICES]
+data.filtered = data.original[, EXPERT.FEATURES]
 
 # > dim(data.original)
-# [1] 5684  102
+# [1] 5684  100
 # > dim(data.filter)
-# [1] 5684   26
+# [1] 5684   24
 
 # -------------------------------------------------------------------------------------------------
 # Preprocessing datasets
@@ -29,11 +34,11 @@ data.filtered = data.original[, EXPERT.CHOICES]
 
 data.original.preproc = DataPreprocessing(data = data.original)
 # > dim(data.preproc)
-# [1] 5684   54
+# [1] 5684   52
 
 data.filtered.preproc = DataPreprocessing(data = data.filtered)
 # dim(data.filtered.preproc)
-# 5684   23
+# 5684   21
 
 # -------------------------------------------------------------------------------------------------
 # Create datasets
--- R/DataPreprocessing.R
+++ R/DataPreprocessing.R
@@ -6,9 +6,12 @@ DataPreprocessing = function(data) {
 	#character to factor 
 	data1 = data 
 	for(cn in colnames(data1)) {
-		if(class(data[,cn]) == "character") {
+		if(class(data1[,cn]) == "character") {
 			data1[,cn] = as.factor(data1[,cn])
-		}
+			
+		} 
+		na.ids = which(data1[,cn] == "")
+		data1[na.ids, cn] = NA	
 	}
 
 	# removing ids and NA features
@@ -20,25 +23,54 @@ DataPreprocessing = function(data) {
 	  	classes = list(
 			factor  = mlr::imputeConstant("NewValue"),
 			integer = mlr::imputeMean(),
+			numeric = mlr::imputeMean(),
 			logical = mlr::imputeMode() 
 	  	)
 	)
-
 	data3 = res$data
 
-	# TODO: remove high correlated features
+	# removing constant featrues after imputation
+	data4 = mlr::removeConstantFeatures(obj = data3 , perc = 0.05, na.ignore = FALSE)
+
+	# removing high correlated features
+	data5 = RemoveHighCorrelatedFeatures(data = data4, perc.cor = 0.9)
 	
-	return(data3)
+	return(data5)
 }
 
 # -------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------
 
-DataPreProcessingNumerical = function(data) {
-	# categoricos -> numerics/binários
-	#obj = mlr::createDummyFeatures(obj = data3, method = "1-of-n")
-	# normalização entre [0,1]
+RemoveHighCorrelatedFeatures = function(data, perc.cor = 0.99) {
+	
+	temp = data
+	for(cn in colnames(data)) {
+		if(class(data[, cn]) != "numeric") {
+			temp[,cn] = as.numeric(data[, cn])
+		}
+	}
+
+	corMatrix = cor(temp)
+
+	# Modify correlation matrix
+	corMatrixRM = corMatrix                  
+	corMatrixRM[upper.tri(corMatrixRM)] = 0
+	diag(corMatrixRM) = 0
+
+	# removing high correlated features
+	dataNew = data[ , !apply(corMatrixRM, 2, function(x) any(x > perc.cor))]
+	return(dataNew)
 }
+
+
+# -------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
+
+# DataPreProcessingNumerical = function(data) {
+# 	# categoricos -> numerics/binários
+# 	#obj = mlr::createDummyFeatures(obj = data3, method = "1-of-n")
+# 	# normalização entre [0,1]
+# }
 	
 # -------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------
